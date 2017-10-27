d5fec9be5a20fc55ba87345744b3ad38f6261855
Markel <markelsanz14@gmail.com>
2017-10-27 15:19:37 -0400

    Consistency fixes

--- get_next_batch.py
+++ get_next_batch.py
@@ -5,37 +5,13 @@ import pandas as pd
 
 def create_connection():
     try:
-        conn = sqlite3.connect("myImages.db")
+        conn = sqlite3.connect("../myImages.db")
         return conn
     except Error as e:
         print(e)
  
     return None
 
-#Reads images from directory and creates the training set
-def get_next_batch(size, category_dict):
-    batch_x = []
-    batch_y = []
-    path = '../train/'
-
-    random_filenames = random.sample([x for x in os.listdir(path) if os.path.isfile(os.path.join(path, x))], size)
-    for im in random_filenames:
-        image = Image.open(im)
-        elems = im.split('-')
-
-        # Add label
-        category = elems[len(elems)-1]
-        label = category_dict[category]
-        label_one_hot = np.zeros(5270)
-        label_one_hot[label] = 1
-        batch_y.append(label_one_hot)
-
-        # Add features flattened
-        image_pixels = np.asarray(image.getdata()).reshape((image.size[1]*image.size[1]*3))
-        batch_x.append(image_pixels)
-
-
-    return batch_x, batch_y
 
 def get_next_training_batch(batch_size, category_to_int):
     conn = create_connection()
--- script_create_db.py
+++ script_create_db.py
@@ -5,6 +5,7 @@ import sqlite3
 path1 = '../train2/'
 def genimages():
     """Generate example images."""
+    print('genimages called')
     i = 0
     for pngpath in glob.iglob(path1+'*.png'):
         with open(pngpath, 'rb') as f:
@@ -12,15 +13,16 @@ def genimages():
             if i % 100 == 0:
                 print(i)
             i += 1
-            yield i, name, buffer(f.read())
+            yield i, name, f.read()
 
 
 try:
     connection = sqlite3.connect('../myImages.db')
     cursor = connection.cursor()
-    cursor.execute("CREATe TABLE IF NOT EXISTS Training(id int, name string, image blob)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS Training(id int, name string, image blob)")
+    print('DB created')
     cursor.executemany("INSERT INTO Training(name,image) values(?,?)", genimages())
-
+    print('DB fully populated')
     connection.commit()
 finally:
     connection.close()
